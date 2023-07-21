bc644325f97326a8cb9473e34ce1067340f9bd42
Almaz Galimov <almazpython@gmail.com>
2023-07-21 17:30:21 +0500

    Добавил виджет для выбора даты вместо поля ввода

--- set_reports/cashierspeed/forms.py
+++ set_reports/cashierspeed/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from django.forms import widgets
+from django.forms.widgets import DateInput
 
 
 class DateForm(forms.Form):
-    analyze_day = forms.DateField(label='Дата')
+    analyze_day = forms.DateField(label='Дата', widget=widgets.DateInput(attrs={'type': 'date', 'style': 'width: 200px;'}))
     filter_shop = forms.BooleanField(required=False, label='Фильтр по магазину')
     shop_num = forms.IntegerField(required=False, label='Номер магазина')
