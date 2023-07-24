4390dbc00b79aa4c569e4b2d808803e9e622d79f
Almaz Galimov <almazpython@gmail.com>
2023-07-24 14:37:23 +0500

    Поправил автоввод номера магазина если выбран фильтр по магазину

--- set_reports/cashierspeed/forms.py
+++ set_reports/cashierspeed/forms.py
@@ -5,5 +5,5 @@ from django.forms import widgets
 class DateForm(forms.Form):
     analyze_day_start = forms.DateField(label='Дата начала', widget=widgets.DateInput(attrs={'type': 'date', 'style': 'width: 200px;'}))
     analyze_day_end = forms.DateField(label='Дата окончания', widget=widgets.DateInput(attrs={'type': 'date', 'style': 'width: 200px;'}))
-    filter_shop = forms.BooleanField(required=False, label='Фильтр по магазину')
-    shop_num = forms.IntegerField(required=False, label='Номер магазина')
+    filter_shop = forms.BooleanField(required=False, label='Фильтр по магазину', widget=forms.CheckboxInput(attrs={'id': 'id_filter_shop'}))
+    shop_num = forms.IntegerField(required=False, label='Номер магазина', widget=forms.NumberInput(attrs={'id': 'id_shop_num'}))
